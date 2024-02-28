a781ea49fca4d2e65aed016bb395435f278830d8
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-02-27 18:02:41 -0800

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+# Ruby script to match the regular expression
+
+regex = /School/  # Define the regular expression to match "School"
+
+input = ARGV[0]   # Get the input argument passed to the script
+
+if input.match?(regex)  # Check if the input matches the regular expression
+  puts input.gsub(regex, 'School') + '$'  # Output the matched string with a "$" at the end
+else
+  puts '$'  # Output "$" if there is no match
+end
+
