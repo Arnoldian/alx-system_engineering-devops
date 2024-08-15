3db6886441bbf686bfc61f16743354352657d0ab
Nkosikhona Arnold Dlamini <nkosikhonadlamini@outlook.com>
2024-08-15 09:57:43 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-Module using method
+Module to query the Reddit API for subscriber count.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Method for queries Reddit API and returns number of
-    subs for specific subreddit"""
+    """Queries the Reddit API and returns the number of subscribers
+    for a specific subreddit. If the subreddit is invalid, it returns 0.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: No. of subs or 0 if subreddit is invalid.
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        return data.get('data', {}).get('subscribers', 0)
     else:
         return 0
