ab428b4b0e2d78fc91c6b06f9ddae997477ba9f1
Nkosikhona Arnold Dlamini <nkosikhonadlamini@outlook.com>
2024-08-15 09:55:10 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Method for querying Reddit API and returning the number of
-    subscribers for a specific subreddit. If the subreddit is invalid,
-    it returns 0.
-    """
+    """Method for queries Reddit API and returns number of
+    subs for specific subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
+        return data.get('data').get('subscribers')
     else:
         return 0
-
