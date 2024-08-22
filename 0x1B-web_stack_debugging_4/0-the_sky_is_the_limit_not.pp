8306c80fcb760feb0fe58bcf1257c517e769d3f6
Nkosikhona Arnold Dlamini <nkosikhonadlamini@outlook.com>
2024-08-22 11:25:46 -0700

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,21 @@
 # Manifest optimizing Nginx config for improved performance
-exec { 'fix--for-nginx':
-  command => '/usr/sbin/nginx -s reload',
-  path    => '/usr/sbin:/usr/bin:/bin',
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+file { '/etc/nginx/nginx.conf':
+  source => 'puppet:///modules/nginx/nginx.conf',
+  notify => Service['nginx'],
+}
+
+exec { 'increase-open-files':
+  command => 'echo "worker_rlimit_nofile 1024;" >> /etc/nginx/nginx.conf',
+  unless  => 'grep -q "worker_rlimit_nofile" /etc/nginx/nginx.conf',
+  notify  => Service['nginx'],
 }
 
---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp-copy
@@ -0,0 +1,6 @@
+# Manifest optimizing Nginx config for improved performance
+exec { 'fix--for-nginx':
+  command => '/usr/sbin/nginx -s reload',
+  path    => '/usr/sbin:/usr/bin:/bin',
+}
+
