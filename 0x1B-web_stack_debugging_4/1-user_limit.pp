e488fc3368800965adcb03d8e550b73609d42d46
Nkosikhona Arnold Dlamini <nkosikhonadlamini@outlook.com>
2024-08-22 11:23:10 -0700

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Manifest modifying user limits for holberton user
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 1024',
-  user    => 'holberton',
+  command => 'ulimit -n 4096 && su - holberton',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
