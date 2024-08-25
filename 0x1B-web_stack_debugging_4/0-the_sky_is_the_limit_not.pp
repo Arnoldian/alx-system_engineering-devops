11b3fdd5707fd41cc2b45b49dea3feed91e2242c
Nkosikhona Arnold Dlamini <nkosikhonadlamini@outlook.com>
2024-08-24 20:13:04 -0700

    tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,6 @@
 # Manifest optimizing Nginx config for improved performance
-package { 'nginx':
-  ensure => installed,
-}
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-file { '/etc/nginx/nginx.conf':
-  source => 'puppet:///modules/nginx/nginx.conf',
-  notify => Service['nginx'],
-}
-
-exec { 'increase-open-files':
-  command => 'echo "worker_rlimit_nofile 1024;" >> /etc/nginx/nginx.conf',
-  unless  => 'grep -q "worker_rlimit_nofile" /etc/nginx/nginx.conf',
-  notify  => Service['nginx'],
-}
-
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Manifest modifying user limits for holberton user
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 4096 && su - holberton',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
 
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
